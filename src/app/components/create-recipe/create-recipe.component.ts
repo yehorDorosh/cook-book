a5d437e0d3d2b09ebd42140800e99019d1950dad
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-04-03 10:27:33 +0100

    Add ingredietns from api

--- src/app/components/create-recipe/create-recipe.component.ts
+++ src/app/components/create-recipe/create-recipe.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   ComponentRef,
+  OnInit,
   ViewChild,
   ViewContainerRef,
 } from '@angular/core';
@@ -9,13 +10,21 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { type Ingredient } from '../ingredient-form/ingredient.model';
 import { v4 as uuidv4 } from 'uuid';
 
+const INGREDIENTS_FROM_API: {
+  [id: string]: Ingredient;
+} = {
+  '1': { id: '1', name: 'Sugar', value: '100g' },
+  '2': { id: '2', name: 'Flour', value: '200g' },
+  '3': { id: '3', name: 'Eggs', value: '2' },
+}
+
 @Component({
   selector: 'app-create-recipe',
   imports: [FormsModule],
   templateUrl: './create-recipe.component.html',
   styleUrl: './create-recipe.component.scss',
 })
-export class CreateRecipeComponent {
+export class CreateRecipeComponent implements OnInit {
   @ViewChild('ingredientsFormList', { read: ViewContainerRef, static: true })
   ingredientsFormList!: ViewContainerRef;
 
@@ -23,10 +32,42 @@ export class CreateRecipeComponent {
     [id: string]: ComponentRef<IngredientFormComponent>;
   } = {};
   ingredientsData: {
-    [key: string]: Ingredient;
+    [id: string]: Ingredient;
   } = {};
 
-  addIngredientForm() {
+  ngOnInit(): void {
+    for (const key in INGREDIENTS_FROM_API) {
+      this.addIngredientForm(INGREDIENTS_FROM_API[key]);
+    }
+  }
+
+  addIngredientForm(ingredient: Ingredient) {
+    const myuuid = uuidv4();
+    const formRef = this.ingredientsFormList.createComponent(
+      IngredientFormComponent
+    );
+    formRef.setInput('id', myuuid);
+    formRef.setInput('ingredient', ingredient);
+
+    this.ingredientsData[myuuid] = { ...ingredient, id: myuuid };
+    this.ingredientRefs[myuuid] = formRef;
+
+    formRef.instance.ingredientInput.subscribe((value: string) => {
+      this.ingredientsData[formRef.instance.id].name = value;
+    });
+
+    formRef.instance.valueInput.subscribe((value: string) => {
+      this.ingredientsData[formRef.instance.id].value = value;
+    });
+
+    formRef.instance.deleteBtn.subscribe((id: string) => {
+      this.ingredientRefs[id].destroy();
+      delete this.ingredientsData[id];
+      delete this.ingredientRefs[id];
+    });
+  }
+
+  addNewIngredientForm() {
     const myuuid = uuidv4();
     const formRef = this.ingredientsFormList.createComponent(
       IngredientFormComponent
--- src/app/components/ingredient-form/ingredient-form.component.ts
+++ src/app/components/ingredient-form/ingredient-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-ingredient-form',
@@ -7,11 +8,23 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './ingredient-form.component.html',
   styleUrl: './ingredient-form.component.scss',
 })
-export class IngredientFormComponent {
+export class IngredientFormComponent implements OnInit {
   @Input() id!: string;
+  @Input() ingredient?: Ingredient;
   @Output() ingredientInput = new EventEmitter<string>();
   @Output() valueInput = new EventEmitter<string>();
   @Output() deleteBtn = new EventEmitter<string>();
+
+  @ViewChild('ingredietnInputRef', {static: true}) private ingredietnInputRef!: ElementRef<HTMLInputElement>;
+  @ViewChild('valueInputRef', {static: true}) private valueInputRef!: ElementRef<HTMLInputElement>;
+
+
+  ngOnInit(): void {
+    if (this.ingredient) {
+      this.ingredietnInputRef.nativeElement.value = this.ingredient.name;
+      this.valueInputRef.nativeElement.value = this.ingredient.value;
+    }
+  }
 
   onIngredientInput(event: Event) {
     const target = event.target as HTMLInputElement;
