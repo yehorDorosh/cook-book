479f34da09e0adb56fecf7deab0b9b29b2c43cb8
Yehor Dorosh <dorosh.y@brainrocket.com>
2025-04-23 13:59:14 +0100

    Add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../secure/firebase';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const app = initializeApp(firebaseConfig);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
